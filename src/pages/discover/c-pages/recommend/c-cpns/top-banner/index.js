9e57f0348fc6879daaf8ab50eb642d59f26e0c62
guoshuanzhu <gszmonster@163.com>
2021-08-25 22:03:21 +0800

    推荐轮播图

--- src/pages/discover/c-pages/recommend/c-cpns/top-banner/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/top-banner/index.js
@@ -1,11 +1,64 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useRef, useCallback, useState } from "react";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { Carousel } from "antd";
 
-
+import { BannerWrapper, BannerLeft, BannerRight, BannerControl } from "./style";
+import * as actions from "../../store/actionCreators";
 
 export default memo(function MKTopBanner() {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const { topBanners } = useSelector(
+    (state) => ({
+      // topBanners: state.get("recommend").get("topBanners")
+      topBanners: state.getIn(["recommend", "topBanners"]),
+    }),
+    shallowEqual
+  );
+  const dispatch = useDispatch();
+
+  const bannerRef =  useRef();
+
+  useEffect(() => {
+    dispatch(actions.getTopBannersAction());
+  }, [dispatch]);
+
+  const bannerChange = useCallback((from, to) => {
+    setCurrentIndex(to);
+  },[])
+
+  const bgImage = topBanners[currentIndex] && topBanners[currentIndex].imageUrl + "?imageView&blur=40x20";
+
   return (
-    <div>
-      
-    </div>
-  )
-})
+    <BannerWrapper bgImage={bgImage}>
+      <div className="banner wrap-v2">
+        <BannerLeft>
+          <Carousel effect="fade" autoplay ref={bannerRef} beforeChange={bannerChange}>
+            {
+              topBanners.map((item) => {
+                return (
+                  <div key={item.targetId}>
+                    <img src={item.imageUrl + "?imageView&quality=89"} className="image" alt={item.typeTitle}/>
+                  </div>
+                );
+              })
+            }
+          </Carousel>
+        </BannerLeft>
+        <BannerRight>
+          <div className="download sprite_download">
+            <a className="sprite_download" href="/">下载客户端</a>
+            <p>PC 安卓 iPhone WP iPad Mac 六大客户端</p>
+            <span className="sprite_banner shadow l"></span>
+            <span className="sprite_banner shadow r"></span>
+          </div>
+        </BannerRight>
+        <BannerControl>
+          <button className="btn left" onClick={e=>{bannerRef.current.prev()}}></button>
+          <button className="btn right" onClick={e=>{bannerRef.current.next()}}></button>
+        </BannerControl>
+      </div>
+    </BannerWrapper>
+  );
+});
--- src/pages/discover/c-pages/recommend/c-cpns/top-banner/style.js
+++ src/pages/discover/c-pages/recommend/c-cpns/top-banner/style.js
@@ -1,17 +1,93 @@
 import styled from "styled-components";
 
 export const BannerWrapper = styled.div`
-
+  background: url(${props => props.bgImage}) center center/6000px;
+  
+  .banner {
+    height: 100%;
+    display: flex;
+    position: relative;
+  }
 `
 
 export const BannerLeft = styled.div`
+  width: 730px;
 
+  .image {
+    height: 285px;
+    width: 730px;
+  }
 `
 
 export const BannerRight = styled.div`
+  height: 100%;
+  flex: 1;
+  position: relative;
 
+  .download {
+    width: 100%;
+    height: 285px;
+    background-position: 0 0;
+    overflow: hidden;
+  }
+
+  a {
+    display: block;
+    width: 215px;
+    height: 56px;
+    margin: 186px 0 0 19px;
+    text-indent: -9999px;
+  }
+  a:hover {
+    background-position: 0 -290px;
+  }
+  p {
+    margin: 10px auto;
+    text-align: center;
+    color: #8d8d8d;
+  }
+  .shadow {
+    display: block;
+    position: absolute;
+    top: 0;
+    width: 20px;
+    height: 285px;
+    .l {
+      left: -20px;
+      background-position: -1px 0;
+    }
+    .r{
+      right: -20px;
+      background-position: -20px 0;
+    }
+  }
 `
 
 export const BannerControl = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  .btn {
+    position: absolute;
+    width: 37px;
+    height: 63px;
+    background-image: url(${require("@/assets/img/banner_sprite.png").default});
+    background-color: transparent;
+    cursor: pointer;
 
+    &:hover {
+      background-color: rgba(0, 0, 0, .1);
+    }
+  }
+
+  .left {
+    left: -68px;
+    background-position: 0 -360px;
+  }
+  .right {
+    right: -68px;
+    background-position: 0 -508px;
+  }
 `
